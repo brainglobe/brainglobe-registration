8960e4d5847466ac358cfbc88de7f890c0c164c4
Adam Tyson <code@adamltyson.com>
2023-03-28 16:37:17 +0100

    Add basic registration

--- src/bg_elastix/_tests/test_widget.py
+++ src/bg_elastix/_tests/test_widget.py
@@ -1,24 +1,9 @@
 import numpy as np
 
-from bg_elastix import ExampleQWidget, example_magic_widget
-
+from bg_elastix.elastix import register
 
 # make_napari_viewer is a pytest fixture that returns a napari viewer object
 # capsys is a pytest fixture that captures stdout and stderr output streams
-def test_example_q_widget(make_napari_viewer, capsys):
-    # make viewer and add an image layer using our fixture
-    viewer = make_napari_viewer()
-    viewer.add_image(np.random.random((100, 100)))
-
-    # create our widget, passing in the viewer
-    my_widget = ExampleQWidget(viewer)
-
-    # call our widget method
-    my_widget._on_click()
-
-    # read captured output and check that it's as we expected
-    captured = capsys.readouterr()
-    assert captured.out == "napari has 1 layers\n"
 
 
 def test_example_magic_widget(make_napari_viewer, capsys):
@@ -26,7 +11,7 @@ def test_example_magic_widget(make_napari_viewer, capsys):
     layer = viewer.add_image(np.random.random((100, 100)))
 
     # this time, our widget will be a MagicFactory or FunctionGui instance
-    my_widget = example_magic_widget()
+    my_widget = register()
 
     # if we "call" this object, it'll execute our function
     my_widget(viewer.layers[0])
