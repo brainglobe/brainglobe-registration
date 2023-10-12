d0bf17c2975362ae1a243440144cee791d147388
IgorTatarnikov <i.tatarnikov@ucl.ac.uk>
2023-10-12 13:26:58 +0100

    Added start alignment button

--- src/bg_elastix/_widget.py
+++ src/bg_elastix/_widget.py
@@ -5,9 +5,9 @@ from magicgui.widgets import FunctionGui, PushButton
 from napari.layers.image import Image
 from napari.utils.events import Event
 import napari
+from bg_atlasapi.list_atlases import get_downloaded_atlases
 
 from bg_elastix.elastix.register import run_registration, get_atlas_by_name
-from bg_atlasapi.list_atlases import get_downloaded_atlases
 
 if TYPE_CHECKING:
     import napari
@@ -21,6 +21,7 @@ def register_widget() -> FunctionGui:
         translate_y=dict(widget_type="SpinBox", min=-5000, max=5000),
         rotate=dict(widget_type="SpinBox", min=-180, max=180),
         get_atlas_button=dict(widget_type="PushButton", text="Get Atlas"),
+        start_alignment_button=dict(widget_type="PushButton", text="Start Alignment"),
         move_sample_button=dict(widget_type="PushButton", text="Adjust Image"),
         reset_button=dict(widget_type="PushButton", text="Reset Image")
     )
@@ -29,6 +30,7 @@ def register_widget() -> FunctionGui:
             atlas_name=None,
             get_atlas_button=False,
             image_to_adjust: "napari.layers.Image" = None,
+            start_alignment_button=False,
             translate_x: int = 0,
             translate_y: int = 0,
             rotate: int = 0,
@@ -38,7 +40,7 @@ def register_widget() -> FunctionGui:
             atlas_image: "napari.layers.Image" = None,
             rigid=True,
             affine=True,
-            bspline=True,
+            bspline=False,
             use_default_params=True,
             affine_iterations="2048",
             log=False,
@@ -59,7 +61,14 @@ def register_widget() -> FunctionGui:
         return [napari.layers.Image(result, name="registered image"),
                 napari.layers.Labels(viewer.layers[-1].data[current_atlas_slice, :, :], name="registered annotations")]
 
-    register.rigid.value = False
+    @register.get_atlas_button.changed.connect
+    def get_atlas_button_click():
+        atlas = get_atlas_by_name(register.atlas_name.value)
+
+        register.viewer.value.add_image(atlas.reference, name=register.atlas_name.value, colormap="red", opacity=0.6,
+                                        blending="translucent")
+        register.viewer.value.add_labels(atlas.annotation, name=register.atlas_name.value + "Annotation", visible=False)
+        register.viewer.value.grid.enabled = True
 
     @register.move_sample_button.changed.connect
     def move_atlas_button_click():
@@ -69,20 +78,20 @@ def register_widget() -> FunctionGui:
 
     @register.reset_button.changed.connect
     def reset_button_on_click():
-        register.image_to_adjust.value.translate = (0, 0)
-        register.image_to_adjust.value.rotate = 0
+        if register.image_to_adjust.value:
+            register.image_to_adjust.value.translate = (0, 0)
+            register.image_to_adjust.value.rotate = 0
 
-        register.translate_x.value = 0
-        register.translate_y.value = 0
-        register.rotate.value = 0
+            register.translate_x.value = 0
+            register.translate_y.value = 0
+            register.rotate.value = 0
 
-    @register.get_atlas_button.changed.connect
-    def get_atlas_button_click():
-        atlas = get_atlas_by_name(register.atlas_name.value)
-
-        register.viewer.value.add_image(atlas.reference, name=register.atlas_name.value, colormap="red", opacity=0.6,
-                                        blending="translucent")
-        register.viewer.value.add_labels(atlas.annotation, name=register.atlas_name.value + "Annotation", visible=False)
-        register.viewer.value.grid.enabled = False
+    @register.start_alignment_button.changed.connect
+    def start_alignment_button_on_clik():
+        if register.image_to_adjust.value:
+            image_to_adjust: "napari.layers.Image" = register.image_to_adjust.value
+            image_to_adjust.opacity = 0.6
+            image_to_adjust.colormap = "green"
+            image_to_adjust.blending = "translucent"
 
     return register
