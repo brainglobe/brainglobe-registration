01db43b93557c3d9343d1f414de015d54132df17
Igor Tatarnikov <igor.tatarnikov@gmail.com>
2024-06-27 16:21:30 +0100

    Add test for reset_atlas in adjust_moving_image_view

--- tests/test_adjust_moving_image_view.py
+++ tests/test_adjust_moving_image_view.py
@@ -141,3 +141,21 @@ def test_atlas_rotation_changed(
         adjust_moving_image_view.adjust_atlas_rotation.click()
 
     assert blocker.args == [10, 20, 30]
+
+
+def test_atlas_reset_button_click(
+    qtbot,
+    adjust_moving_image_view,
+):
+    qtbot.addWidget(adjust_moving_image_view)
+
+    with qtbot.waitSignal(
+        adjust_moving_image_view.atlas_reset_signal, timeout=1000
+    ):
+        adjust_moving_image_view.atlas_reset_button.click()
+
+    assert (
+        adjust_moving_image_view.adjust_atlas_pitch.value() == 0
+        and adjust_moving_image_view.adjust_atlas_yaw.value() == 0
+        and adjust_moving_image_view.adjust_atlas_roll.value() == 0
+    )
