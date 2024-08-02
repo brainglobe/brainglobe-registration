5b1eb883cb4a8c6aad213d14349bafc4d2ab1427
Igor Tatarnikov <igor.tatarnikov@gmail.com>
2024-08-02 15:43:52 +0100

    Fixed sample data loading

---
+++ brainglobe_registration/sample_data.py
@@ -0,0 +1,21 @@
+from importlib.resources import files
+from typing import List
+
+from napari.types import LayerData
+from tifffile import imread
+
+
+def load_sample_data() -> List[LayerData]:
+    """
+    Load the sample data.
+
+    Returns
+    -------
+    List[LayerData]
+        The sample data.
+    """
+    path = str(
+        files("brainglobe_registration").joinpath("resources/sample_hipp.tif")
+    )
+
+    return [(imread(path), {"name": "Sample Data"})]
