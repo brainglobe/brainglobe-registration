de184651d58f296908dd77de3f84bcad2a585402
saarah815 <saarah.815@gmail.com>
2025-06-10 19:07:42 +0100

    remove_atlas_rotation_from_registration_widget

--- brainglobe_registration/registration_widget.py
+++ brainglobe_registration/registration_widget.py
@@ -43,6 +43,11 @@ from skimage.segmentation import find_boundaries
 from skimage.transform import rescale
 from tifffile import imwrite
 
+from brainglobe_registration.utils.atlas_rotation import (
+    on_adjust_atlas_rotation,
+    compute_atlas_rotation,
+)
+
 from brainglobe_registration.utils.utils import (
     calculate_region_size,
     calculate_rotated_bounding_box,
@@ -66,6 +71,8 @@ from brainglobe_registration.widgets.transform_select_view import (
 
 
 class RegistrationWidget(QScrollArea):
+    on_adjust_atlas_rotation = on_adjust_atlas_rotation
+    compute_atlas_rotation = compute_atlas_rotation
     def __init__(self, napari_viewer: Viewer):
         super().__init__()
         self._widget = CollapsibleWidgetContainer()
@@ -720,86 +727,6 @@ class RegistrationWidget(QScrollArea):
             preserve_range=True,
             anti_aliasing=True,
         ).astype(self._moving_image_data_backup.dtype)
-
-    def _on_adjust_atlas_rotation(self, pitch: float, yaw: float, roll: float):
-        if not (
-            self._atlas
-            and self._atlas_data_layer
-            and self._atlas_annotations_layer
-        ):
-            show_error(
-                "No atlas selected. Please select an atlas before rotating"
-            )
-            return
-
-        # Create the rotation matrix
-        roll_matrix = active_matrix_from_angle(0, np.deg2rad(roll))
-        yaw_matrix = active_matrix_from_angle(1, np.deg2rad(yaw))
-        pitch_matrix = active_matrix_from_angle(2, np.deg2rad(pitch))
-
-        # Combine rotation matrices
-        rotation_matrix = yaw_matrix @ pitch_matrix @ roll_matrix
-
-        full_matrix = np.eye(4)
-        full_matrix[:3, :3] = rotation_matrix
-
-        # Translate the origin to the center of the image
-        origin = np.asarray(self._atlas.reference.shape) / 2
-        translate_matrix = np.eye(4)
-        translate_matrix[:-1, -1] = -origin
-
-        bounding_box = calculate_rotated_bounding_box(
-            self._atlas.reference.shape, full_matrix
-        )
-        new_translation = np.asarray(bounding_box) / 2
-        post_rotate_translation = np.eye(4)
-        post_rotate_translation[:3, -1] = new_translation
-
-        # Combine the matrices. The order of operations is:
-        # 1. Translate the origin to the center of the image
-        # 2. Rotate the image
-        # 3. Translate the origin back to the top left corner
-        self._atlas_transform_matrix = np.linalg.inv(
-            post_rotate_translation @ full_matrix @ translate_matrix
-        )
-
-        self._atlas_data_layer.data = dask_affine_transform(
-            self._atlas.reference,
-            self._atlas_transform_matrix,
-            order=2,
-            output_shape=bounding_box,
-            output_chunks=(2, bounding_box[1], bounding_box[2]),
-        ).astype(self._atlas.reference.dtype)
-
-        self._atlas_annotations_layer.data = dask_affine_transform(
-            self._atlas.annotation,
-            self._atlas_transform_matrix,
-            order=0,
-            output_shape=bounding_box,
-            output_chunks=(2, bounding_box[1], bounding_box[2]),
-        ).astype(self._atlas.annotation.dtype)
-
-        # Resets the viewer grid to update the grid to the new atlas
-        # The grid is disabled and re-enabled to force the grid to update
-        self._viewer.reset_view()
-        self._viewer.grid.enabled = False
-        self._viewer.grid.enabled = True
-
-        worker = self.compute_atlas_rotation(self._atlas_data_layer.data)
-        worker.returned.connect(self.set_atlas_layer_data)
-        worker.start()
-
-    @thread_worker
-    def compute_atlas_rotation(self, dask_array: da.Array):
-        self.adjust_moving_image_widget.reset_atlas_button.setEnabled(False)
-        self.adjust_moving_image_widget.adjust_atlas_rotation.setEnabled(False)
-
-        computed_array = dask_array.compute()
-
-        self.adjust_moving_image_widget.reset_atlas_button.setEnabled(True)
-        self.adjust_moving_image_widget.adjust_atlas_rotation.setEnabled(True)
-
-        return computed_array
 
     def set_atlas_layer_data(self, new_data):
         self._atlas_data_layer.data = new_data
